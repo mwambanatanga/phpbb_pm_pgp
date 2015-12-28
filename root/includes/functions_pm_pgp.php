91ea0cd459682d0de814f1f39306cc902df99685
Mwamba Natanga <mwambanatanga@gmail.com>
2015-12-28 19:01:31 +0900

    New fields in pgp_keys DB, more info about PGP key in UCP, complete en and ru translations

--- root/includes/functions_pm_pgp.php
+++ root/includes/functions_pm_pgp.php
@@ -12,39 +12,7 @@ if (!defined('IN_PHPBB'))
 	exit; 
 }
 $user->add_lang('mods/pm_pgp');
-/*
-// remove a user's thanks
-function delete_pgp_key($user_id)
-{
-	global $db, $user, $phpbb_root_path, $phpEx, $forum_id, $config, $auth;
-	// confirm
-	
-	if (confirm_box(true, 'REMOVE_PGP_KEY', $hidden = ''))
-	{
-		if (!defined('PGP_KEYS_TABLE'))
-		{
-			define('PGP_KEYS_TABLE', $table_prefix . 'pgp_keys');
-		}
-		$sql = "DELETE FROM " . PGP_KEYS_TABLE . "
-				WHERE user_id = " . (int) $user->data['user_id'];
-			$db->sql_query($sql);
-			$result = $db->sql_affectedrows($sql);
-			if ($result != 0)
-			{
-				$sql = "UPDATE " .$table_prefix . "users" . " 
-						SET user_has_pgp_key = 0 
-						WHERE user_id = " . (int) $user->data['user_id'];
-					$db->sql_query($sql);
-					$result = $db->sql_affectedrows($sql);
-			}
-	}
-	else
-	{
-		confirm_box(false, 'REMOVE_PGP_KEY', $hidden = '');
-	}
-	return;
-}
-*/
+
 // get user's key fingerprint
 function get_pgp_fingerprint($user_id)
 {
@@ -74,8 +42,14 @@ function user_has_pgp_key($user_id)
 	return $result;
 }
 
-function get_pgp_public_key($user_id)
+function get_pgp_data($user_id)
 {
+	$pgp_data = array(
+		'gpg_key' => '',
+		'fingerprint' => '',
+		'key_user' => '',
+		'key_type' => '',
+	);
 	global $db, $user;
 	if (!defined('PGP_KEYS_TABLE'))
 	{
@@ -83,20 +57,21 @@ function get_pgp_public_key($user_id)
 	}
 	if (user_has_pgp_key($user_id))
 	{
-		$sql = "SELECT pgp_key FROM " . PGP_KEYS_TABLE .  " WHERE user_id = " . $user_id;
-		$db->sql_query($sql);
-		$public_key = $db->sql_fetchfield('pgp_key');
+		$sql = "SELECT * FROM " . PGP_KEYS_TABLE .  " WHERE user_id = " . $user_id;
+		$result = $db->sql_query($sql);
+		$pgp_data = $db->sql_fetchrow($result); // only 1 row is expected
 	}
-	else $public_key = NULL;
-	return $public_key;
+//	else $public_key = NULL;
+	return $pgp_data;
 }
 
 function download_pgp_public_key($user_id)
 {
 	if (user_has_pgp_key($user_id))
 	{
-		$pgp_public_key = get_pgp_public_key($user_id);
-		$pgp_fingerprint = get_pgp_fingerprint($user_id);
+		$pgp_data = get_pgp_data($user_id);
+		$pgp_public_key = $pgp_data['pgp_key'];
+		$pgp_fingerprint = $pgp_data['fingerprint'];
 		header('Content-Disposition: attachment; filename="' . strtoupper(substr($pgp_fingerprint, -8)) . '.key"');
 		header('Content-Type: text/plain');
 		header('Content-Length: ' . strlen($pgp_public_key));
@@ -105,7 +80,7 @@ function download_pgp_public_key($user_id)
 	}
 }
 
-function put_pgp_key($data)
+function put_pgp_data($data)
 //$user_id, $fingerprint, $public_key)
 {
 	global $db, $user;
@@ -128,6 +103,8 @@ function put_pgp_key($data)
 		$sql_ary = array(
 			'fingerprint'		=> $data['fingerprint'],
 			'pgp_key'		=> $data['pgp_key'],
+			'key_user'		=> $data['key_user'],
+			'key_type'		=> $data['key_type'],
 			);
 		$sql = "UPDATE " . PGP_KEYS_TABLE . " SET " . $db->sql_build_array('UPDATE', $sql_ary) .
 			" WHERE user_id = " . $data['user_id'];
@@ -142,6 +119,8 @@ function put_pgp_key($data)
 			'user_id'	=> $data['user_id'],
 			'fingerprint'	=> $data['fingerprint'],
 			'pgp_key'	=> $data['pgp_key'],
+			'key_user'		=> $data['key_user'],
+			'key_type'		=> $data['key_type'],
 			);
 		$sql = "INSERT into " . PGP_KEYS_TABLE . " " . $db->sql_build_array('INSERT', $sql_ary);
 		$db->sql_query($sql);
--- root/install_pm_pgp_mod.php
+++ root/install_pm_pgp_mod.php
@@ -77,15 +77,17 @@ $versions = array(
 	// Now to add a table (this uses the layout from develop/create_schema_files.php and from phpbb_db_tools)
 		'table_add' => array(
 			array(
-                $table_prefix . 'pgp_keys',
-                array(
-                    'COLUMNS'		=> array(
-                        'user_id'		=> array('INT:0', 0),
-                        'pgp_key'		=> array('TEXT', ''),
-                        'fingerprint'		=> array('CHAR:40', ''),
-                    ),
-                    'PRIMARY_KEY'	=> 'user_id',
-                ),
+				$table_prefix . 'pgp_keys',
+				array(
+					'COLUMNS'		=> array(
+						'user_id'		=> array('INT:0', 0),
+						'pgp_key'		=> array('TEXT', ''),
+						'fingerprint'	=> array('CHAR:40', ''),
+						'key_user'		=> array('CHAR:255', ''),
+						'key_type'		=> array('CHAR:10', ''),
+					),
+					'PRIMARY_KEY'	=> 'user_id',
+				),
 			),
 		),
 
@@ -93,94 +95,15 @@ $versions = array(
 		'table_column_add' => array(
 			array($table_prefix . 'users', 'user_has_pgp_key', array('BOOL', 0)),
 		),
-/**
-        'table_row_update' => array(
-            array($table_prefix . 'users',
-                array('user_id' => '55'),
-                array('user_has_pgp_key' => '1'),
-            ),
-        ),
-
-        'table_row_insert' => array(
-            array($table_prefix . 'pgp_keys',
-                array(
-                    'user_id' => '55',
-                    'pgp_key' => "-----BEGIN PGP PUBLIC KEY BLOCK-----
- 
-mQINBAAAAC4BEACxkSFgSCN0Alc/DZxj3u+5lFKucoMpaNRRTZWs/z8VaaWAUo9/pjmflRmr
-/VowsVn4CHzYf+o6wURF7iJ0k/0zuH0aKRPBjwrielJjMqltwlbbZqvpgEFkzrqbvjwQrj1m
-ZRiYS+7LwDK7WDmLb4yNZ7tGkFcmqT8I48tCThZW5da+dAunL/+ny7rALNxArIq0MtGI0WgB
-3Ig7PyiBX1P+0/ICjUH3CRIZQFiLz6wvbzcylSHeks2dSBcLOZ++SkQkKHH/M16qvZ/CTYVA
-hQO8aSHvwyCkFEtSnxSOsiMiHMfu0g+kmjZ7vRLzpm2PwYRh2YB6px0zh/NBctsFc+cnifd6
-f0noVRE0bUWS5pr7KJKF0vDBt2n4tkXU6w+81jtAwOVUesUeaU2+TSwu14UIBTQLnxpyl9ua
-fZOwuk5I4nfaT0v6Cbh57r5HfCVav9wrE59qghkO8TqOjCm1CO1qXZzYQz26L3UbJBsGiu8E
-QIuf/vp2FpNKYnfuDXjRbEMKmKXfAQLbwVGiSo5j4n6Ndf031Wd6rPoweYx9fCA+zSjq/+8X
-H1PfRNYnggwaJrXHtkA7D8NfPs38tV3eFnkwC2KLe/oT1RiM9ugoUsU5lq4qBKbPvV8n4CEH
-ThJGUzTTl8KnCAHzd8QjA8p5Hh+kAryd2mrfLstK7aAe30yh4QARAQABtB5UaGUgSm9rZXIg
-PHRoZS1qb2tlckB4YWtlcC5ydT6JASIEEAECAAwFAlORmTEFAwASdQAACgkQlxC4m8pXrXyx
-wAgAkR72ZP9PCHtTG7DQROpeNEHhnQuLvNh3GMuCekkPtkreFY1mpkvSkNbzUNr/Kk9to9cg
-xYqmEfrx3Ct+c2fgmYFu9fKguScqOfG4B2eQ5e/QrroSpot2mwrMhXG16bt0LRyZDDtY0f1r
-AJavJPJ3ppTvdKRBZh1Ti+298UWo64o0wCPE2umFFMjMbUVoHyqplvtM1zGM1yJ+sRGDWAJL
-KVpJHy9zTpx5nKiaMDAeHFqXX5D8Eso8lXz8MzAD/ssc5IccZev4K5Ea+jw/zqcQepk9YnLI
-i979t2ORkX4Wa6/vltuqL/O2D/AEfLei6iucovD2fO18Mt1gDgHP58JfPIkBIgQQAQIADAUC
-VB3UxAUDABJ1AAAKCRCXELibyletfEsuCADC3nGnXeVjWYt4k68Wpc9NFHAh/O0dxDzz90yq
-6trZDJ8McbOCQfJTJnM+VCKZIcD3uXJS5pKF2eVY3re4u1Y9mUFuaACU76jHzJO9Fa6mjMvD
-SGnn9Ab1v3xSPqfQ4lT1O9lJ8SOO0puCwB7MvV/kjSPpFk7SYxiF8S+KxCT9RLjahRjjvqLH
-3sSSMDT3hx7tU/pRn9rU6pRuNzh95nfTS/qLiZnlVuct8K3UnuJR1D1/rsXwNYrHlJsw2N+r
-zgCDhnTJOl37iXjcjs72hFFM16SJj9ZzlJgrlTP15GLSq00/cg1JPbiMdtwm8wVgPfMxzYi2
-7LuGxo+dShMEB/hUiQEiBBABAgAMBQJVOKGGBQMAEnUAAAoJEJcQuJvKV618jNkH/ArVWXRL
-KQgzPE6RJNEKn9XMtV5SXA/dE4Tksgiyg/4+q16fEGQJs4vUaG2LXoF9EtS5rkauJuGDrLEn
-AWooZQy5XGYIOlsJyD8BwDCEQWKQwtVr3KuG7zbArwmGUUgy4BVSEDZUV9pHEX9ynWTBWGzZ
-efrTay2klFW85aeYfMlTFlYgGBOv3Pt8yKWu5UI99Fm+HTvtB/6uk/ILdIOTUsuE7XPyVjd/
-kDWKxgIAmk9Fg8teV51vyW5kDZ9+MH16hs5kx7ZT5/c1bE4L+4XFAWFPwHd681zvoVwk3nmj
-cU31NRAZhG8XxZ05D0weIH795DXCfsvT95T/fKZXYQH/mrOJAjgEEwECACIFAgAAAC4CGwMG
-CwkIBwMCBhUIAgkKCwQWAgMBAh4BAheAAAoJEI1ee4Q0WHJdVUoP/itGsI0YOBw+Cfge6YcK
-xShiE9WfKY6hio/6bajSrjEkwGQE+71e/cN3fVh+6md/nUX0SzhUpKXyp5DBI7AgVNcIGNCY
-gMbciwA1vLQv8aOp0qZeUSxjalW3UrKQ6baq3xHqev/orWfASPawzJgYQQVVCRVm7NtZGsTi
-iy540SCiJA5l/J1TC8ijq3ql1bcTCLFLLaP9f/byrcq1pctE9eNi5+00Mt6gHm9SIOPeOuWJ
-FtvyTL2Ad3PncaZWm4UvYAICXjLD6UPpfUeWxiGb699QI9paKS36XYddSkZD58myZyCwd29l
-9aF0spXSRGqSH5/0Hmq6RONBmTlxDrJ3S9ajItChuM12matgE3YYOR3xAlxNCjKgEYfyt/NM
-dDTBS2GtHQmxo33dWrvpFZgkXVEzOJpr3uul9iQUiFS7ImWU7zvEp6PYQ66hNo23DZf6sdHF
-pLBBikC02/6MH0r9U8jiBoxZI21p8dqBtekGuga1NCvqn+7jUnCpLZIGgpJqqf8RmNgR+abc
-rumPRBzz6W6vHKLwBceDMe2AlOIyAZ6AG2oC3vpxA7sQvzj71mZ1MS83F/Cs6bmuWGSe0JoB
-XNgdU4IHUvpmzTMdkOnwEqe0ZIHEvhQfIo8Ja7HZt/BQDTOfHE/ZnbpQs3uU0P28p2xrxWoF
-cOB0T245GMHRes2FuQINBAAAAC4BEADUxFbiC259WI5bw6X8ncERbvB2oZxj7IxJPp0H31jj
-eDIIBBXDGS/H9ueXQzs5af139BasJriNqjn8yk4IF7WSdsCTA608qizmKae/rxsbPBVphP8S
-fswJ8zZiQ8QsRmTEtc7rvdpxoTXwGHb7ZgzJvwjh73cT2Cmo0+v4hZALNxryBW3LX/1BXB26
-hAyzGZAvDs5sSy4jL/vbjsqFH3HfzAFxGGu1twE2r6w5ncjYsem2wePBUm0loP2i6zBIw2oP
-kdy6Wv3m4BUQ0srJGjEMxX5UB0Cf87LDBYh3IYScybMfJ0Mx3tlpdglUFUJbAExqKEWCAbaA
-kf+4m5zHl0HtxuM7fNgrkyiPt/oJgjWQd3Qeyxda6ezzKIw+DTObcByqM2rsQCcKqcm8SsYf
-8ZVm1V7c7VR4jAOOrV2Z8atkHxYkpJ3F10X80veVX1hChYUxGv5KXT/6kYqk+Ub37PEvRA9C
-+cvRgMM7dOb0qt8LikD+A9/hXyKTf8cIXZUoA0PEpHEMchD12sQ52lYFbCXJsbiQazGp+7z3
-JW6HZySjRV6NXTBXE9LCnQjCG4rTj/p4Lup6V2CJ480kKYmJOfClKUnwP0Gb4KI6pXboDaOK
-XIw1bYmwhCCQYJZgvkwl+JmveHw5X63AKVEIxlbisszKLIItbtrfLFRjmHmR42D90QARAQAB
-iQIfBBgBAgAJBQIAAAAuAhsMAAoJEI1ee4Q0WHJdjp8QAJuaMu8mRaP9K+5NxtnZC9ZzqBiZ
-vyMs4GyHuwjHpsDNRE/g2PFxpJ4YrS97TduTgjNiVGxCqCC6TSs3YqWxN2hSL0osvdAgQLPy
-Xn5U40awEtO74h1cZZEREGiFcw9zycoAF0/F1z+CEzL8Vs+NF0rknekdNHHgcOqu1Fr5Rv7U
-c/O21OQ84EXb8kZVWhFemXfeaTJOMunGyoekTse1TgEcXRsafp8LOWXntfJaFZJzVIo8wCs0
-suSessckm4Xb6VaOuQSCgxwaiiM8LzmJj3/dq/M76DsHlQlHd3MH0dyJBOWS7Xl7EXDIm3zA
-KkC43RrLmPJrRQorv3CBPtOnNdd8x7XL97PunV3gd0ok13hc6XGNn2ZyApfW+DnYL3bVSwRI
-Cm381L/2Ysm+qjm4mDSdpSvuzmVeriLsjHf7igVFsf3I/IaprYVAAtT8XSiyBwE8w2kuMyFD
-QsaqUlVqO3VZZO8J0qwO00rZxOFeBiK9/95N8r78g4w+CdLctxpFg4OLqnJscnolCycgx/zZ
-+l+mZCK8iwcRplZqy/nBikMoOgAKPJlhNhvdEIJX2HcqMLzxAfQYfvhGlDTsf9b8adENYJv6
-/YUvvZbFXh6Bn7YdgzGjVYMgXDaCNSSCynbiLNh8tpSVLyQdG8NNmFzEFzCK0hnkEZdMQNqW
-gU9U73K1
-=9/hP
------END PGP PUBLIC KEY BLOCK-----",
-                    'fingerprint' => '4B131F4B8E28317E8D72F9788D5E7B843458725D',
-                ),
-            ),
-        ),
-**/
-        'module_add' => array(
-            array('ucp', 'UCP_PROFILE',
-                array( // Mode title
-                    'module_basename'    => 'profile', //info file name minus ucp_
-                    'modes'                => array('pgp_key'), // Mode set in info file
-                ),
-            ),
-        ),
-        'cache_purge' => array(
+		'module_add' => array(
+			array('ucp', 'UCP_PROFILE',
+				array( // Mode title
+					'module_basename'    => 'profile', //info file name minus ucp_
+					'modes'                => array('pgp_key'), // Mode set in info file
+				),
+			),
+		),
+		'cache_purge' => array(
 			array('template', 0),
 		),
 	),
